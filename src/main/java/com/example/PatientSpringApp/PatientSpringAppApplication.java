5c7a25f90dd8f1b1693e198b0ab5dcf2ba8f8cd7
melvinsibichen <melvinsibichen1020@gmail.com>
2022-12-09 01:01:38 +0530

    properties

---
+++ src/main/java/com/example/PatientSpringApp/PatientSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.PatientSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PatientSpringApp/PatientSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PatientSpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
